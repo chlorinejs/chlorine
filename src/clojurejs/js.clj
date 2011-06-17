997905166c087c22f0a14ea0caf1fd18455ddc3b
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-06-17 18:51:26 +0200

    - more clojure 1.3 fixes (mainly dynamic bindings)

--- src/clojurejs/js.clj
+++ src/clojurejs/js.clj
@@ -18,20 +18,20 @@
 
 (defn- re? [expr] (= (class expr) java.util.regex.Pattern))
 
-(def *inline-if* false)
-(def *quoted* false)
+(def ^:dynamic *inline-if* false)
+(def ^:dynamic *quoted* false)
 
-(def *print-pretty* false)
+(def ^:dynamic *print-pretty* false)
 (defmacro with-pretty-print [& body]
   `(binding [*print-pretty* true]
      ~@body))
 
-(def *indent* 0)
+(def ^:dynamic *indent* 0)
 (defmacro with-indent [[& increment] & body]
   `(binding [*indent* (+ *indent* (or ~increment 4))]
      ~@body))
 
-(def *in-block-exp* false)
+(def ^:dynamic *in-block-exp* false)
 (defmacro with-block [& body]
   `(binding [*in-block-exp* true]
      ~@body))
@@ -137,7 +137,7 @@
   (with-parens []
     (with-indent [] (emit-delimited ", " args))))
 
-(def *return-expr* false)
+(def ^:dynamic *return-expr* false)
 (defmacro with-return-expr [[& [new-val]] & body]
   `(binding [*return-expr* (if *return-expr*
                              (do
@@ -146,7 +146,7 @@
                              (or ~new-val false))]
      ~@body))  
 
-(def *in-fn-toplevel* true)
+(def ^:dynamic *in-fn-toplevel* true)
 (defn- emit-function-form [form]
   (binding [*inline-if* true
             *in-fn-toplevel* false]
@@ -191,7 +191,7 @@
   (binding [*inline-if* true]
     (emit value)))
 
-(def *macros* (ref {}))
+(def ^:dynamic *macros* (ref {}))
 (defn- macro? [n] (and (symbol? n) (contains? @*macros* (name n))))
 (defn- get-macro [n] (and (symbol? n) (get @*macros* (name n))))
 (defn- undef-macro [n]
@@ -225,7 +225,7 @@
 (defn- ignorable-arg? [n]
   (and (symbol? n) (.startsWith (name n) "_")))
 
-(def *temp-sym-count* nil)
+(def  ^:dynamic *temp-sym-count* nil)
 (defn tempsym []
   (dosync
    (ref-set *temp-sym-count* (+ 1 @*temp-sym-count*))
@@ -495,7 +495,7 @@
                 (newline-indent)
                 (print "}")))))
 
-(def *loop-vars* nil)
+(def ^:dynamic *loop-vars* nil)
 (defmethod emit "loop" [[_ bindings & body]]
   (let [emit-for-block (fn []
                          (print "for (var ")
