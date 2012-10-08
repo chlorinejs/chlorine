ae456712e8658dcdafd2dbf041ede958781afd59
Hoàng Minh Thắng <p@banphim.net>
2012-10-08 11:53:54 +0700

    add functions test

--- test/clojurejs/js_test.clj
+++ test/clojurejs/js_test.clj
@@ -19,3 +19,52 @@
   (is (= (js #"^([a-z]*)([0-9]*)") "/^([a-z]*)([0-9]*)/"))
   (is (= (js \newline) "'\n'"))
   (is (= (js \a) "'a'")))
+
+(deftest functions
+  (is (= (js (+ 1 2 3)) "(1 + 2 + 3)"))
+  (is (= (js (+ "foo" "bar" "baz")) "(\"foo\" + \"bar\" + \"baz\")"))
+  (is (= (js (:test {:test 1 :foo 2 :bar 3}))
+         "{'test' : 1,'foo' : 2,'bar' : 3}['test']"))
+  (is (= (js (let [m {:test 1 :foo 2 :bar 3}] (:baz m 4)))
+         (str "var m = {'test' : 1,'foo' : 2,'bar' : 3};"
+              " ('baz' in m ? m['baz'] : 4);")))
+  (is (= (js (append '(:foo bar baz) '(quux)))
+         "append(['foo','bar','baz'], ['quux'])"))
+
+  (is (= (js (fn [a b] (+ a b)))
+         "function (a, b) { return (a + b); }"))
+
+  (is (= (with-pretty-print (js (fn "Some func does stuff" [x] (+ x 1))))
+         (str "function (x) {\n"
+              "    /* Some func does stuff */\n"
+              "    return (x + 1);\n}")))
+
+  (is (= (with-pretty-print (js (fn "Some func\ndoes stuff" [x] (+ x 1))))
+         (str "function (x) {\n"
+              "    /* Some func\n"
+              "       does stuff */\n"
+              "    return (x + 1);\n}")))
+
+  (is (= (js (defn foo [a b] (+ a b)))
+         "foo = function (a, b) { return (a + b); }"))
+
+  (is (= (js (defn foo [c] (.methodOf c)))
+         "foo = function (c) { return c.methodOf(); }"))
+
+  (is (= (js
+          (defn test []
+            (let [a 1] (log (* a a)))
+            (let [a 2] (log (* a a)))))
+         (str "test = function () { var a = 1;"
+              " log((a * a));;"
+              " var a = 2;"
+              " return log((a * a));; }")))
+  (is (= (js
+          (defn test []
+            (let [a 1] (log (* a a)))
+            (do (log "test") (+ 1 1))))
+         (str "test = function () {"
+              " var a = 1;"
+              " log((a * a));;"
+              "  log(\"test\");"
+              " return (1 + 1);; }"))))
