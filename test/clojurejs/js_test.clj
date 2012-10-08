a77b2e668dd652a1741a237ffecbeeb8d96944ff
Hoàng Minh Thắng <p@banphim.net>
2012-10-08 12:11:38 +0700

    add destructuring test

--- test/clojurejs/js_test.clj
+++ test/clojurejs/js_test.clj
@@ -83,3 +83,145 @@
 
   (is (= (js (get map .key default))
          "('key' in map ? map.key : default)")))
+
+(deftest destructuring
+  (is (= (js
+          (defn test []
+            (let [a 1
+                  b (+ a 1)
+                  c (+ b 1)]
+              (+ a b c))))
+         (str "test = function () {"
+              " var a = 1, b = (a + 1), c = (b + 1);"
+              " return (a + b + c);; }")))
+
+  ;; & rest
+  (is (= (js
+          (defn test []
+            (let [[a b & r] [1 2 3 4]]
+              [(+ a b) r])))
+         (str "test = function () {"
+              " var _temp_1000 = [1,2,3,4],"
+              " a = _temp_1000[0],"
+              " b = _temp_1000[1],"
+              " r = _temp_1000.slice(2);"
+              " return [(a + b),r];; }")))
+
+  (is (= (js
+          (defn test [[a b & r]]
+            [(+ a b) r]))
+         (str "test = function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " _temp_1001 = _temp_1000[0],"
+              " a = _temp_1001[0],"
+              " b = _temp_1001[1],"
+              " r = _temp_1001.slice(2);"
+              " return [(a + b),r]; }")))
+
+  (is (= (js
+          (defn test [a b & r]
+            [(+ a b) r]))
+         (str "test = function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " a = _temp_1000[0],"
+              " b = _temp_1000[1],"
+              " r = _temp_1000.slice(2);"
+              " return [(a + b),r]; }")))
+
+  ;; :as
+  (is (= (js
+          (fn [a [b] [c d & e :as f] :as g] nil))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " a = _temp_1000[0],"
+              " _temp_1001 = _temp_1000[1],"
+              " b = _temp_1001[0],"
+              " _temp_1002 = _temp_1000[2],"
+              " c = _temp_1002[0],"
+              " d = _temp_1002[1],"
+              " e = _temp_1002.slice(2),"
+              " f = _temp_1002,"
+              " g = _temp_1000;"
+              " return null; }")))
+
+  ;; map destructuring
+  (is (= (js
+          (fn [x {y :y, fred :fred}] fred))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " x = _temp_1000[0],"
+              " _temp_1001 = _temp_1000[1],"
+              " y = _temp_1001['y'],"
+              " fred = _temp_1001['fred'];"
+              " return fred; }")))
+
+  (is (= (js
+          (fn [[{x :x, {z :z} :y}]] z))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " _temp_1001 = _temp_1000[0],"
+              " _temp_1002 = _temp_1001[0],"
+              " x = _temp_1002['x'],"
+              " _temp_1003 = _temp_1002['y'],"
+              " z = _temp_1003['z'];"
+              " return z; }")))
+
+  ;; numbers as keys (this actually works)
+  (is (= (js
+          (fn [{a 1, b 2, :or {a 3}}]))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " _temp_1001 = _temp_1000[0],"
+              " a = (1 in _temp_1001 ? _temp_1001[1] : 3),"
+              " b = _temp_1001[2];"
+              " return null; }")))
+
+  ;; :keys, :strs
+  (is (= (js
+          (fn [x {y :y, z :z :keys [a b]}] z))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " x = _temp_1000[0],"
+              " _temp_1001 = _temp_1000[1],"
+              " a = _temp_1001['a'],"
+              " b = _temp_1001['b'],"
+              " y = _temp_1001['y'],"
+              " z = _temp_1001['z'];"
+              " return z; }")))
+
+  (is (= (js
+          (fn [x {y :y, z :z :strs [a b]}] z))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " x = _temp_1000[0], _temp_1001 = _temp_1000[1],"
+              " a = _temp_1001['a'],"
+              " b = _temp_1001['b'],"
+              " y = _temp_1001['y'],"
+              " z = _temp_1001['z'];"
+              " return z; }")))
+                                        ; defaults
+  (is (= (js
+          (fn [x {y :y, z :z :or {y 1, z "foo"}}] z))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " x = _temp_1000[0],"
+              " _temp_1001 = _temp_1000[1],"
+              " y = ('y' in _temp_1001 ? _temp_1001['y'] : 1),"
+              " z = ('z' in _temp_1001 ? _temp_1001['z'] : \"foo\");"
+              " return z; }")))
+
+  (is (= (js
+          (fn [x {y :y, z :z :keys [a b] :or {a 1, y :bleh}}] z))
+         (str "function () {"
+              " var _temp_1000 = Array.prototype.slice.call(arguments),"
+              " x = _temp_1000[0],"
+              " _temp_1001 = _temp_1000[1],"
+              " a = ('a' in _temp_1001 ? _temp_1001['a'] : 1),"
+              " b = _temp_1001['b'],"
+              " y = ('y' in _temp_1001 ? _temp_1001['y'] : 'bleh'),"
+              " z = _temp_1001['z']; return z; }")))
+
+  ;; unsupported for now
+  (is (thrown-with-msg? Exception #"& must be followed by"
+        (js
+         (fn [x y & {z :z}] z)))))
