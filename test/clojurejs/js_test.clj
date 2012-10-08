bcf16745f1b3e115c1afd94c62299b68162d6f25
Hoàng Minh Thắng <p@banphim.net>
2012-10-08 11:52:50 +0700

    add literals test

--- test/clojurejs/js_test.clj
+++ test/clojurejs/js_test.clj
@@ -5,3 +5,17 @@
 (deftest unzip-test
   (is (= (unzip [:foo 1 :bar 2 :baz 3])
          [[:foo :bar :baz] [1 2 3]])))
+
+(tojs "src/clojurejs/boot.cljs")
+
+(deftest literals
+  (is (= (js *print-pretty*) "__print_pretty__"))
+  (is (= (js number?) "numberp"))
+  (is (= (js foo-bar-baz) "foo_bar_baz"))
+  (is (= (js inc!) "incf"))
+  (is (= (js {:foo 1 :bar 2 :baz 3}) "{'foo' : 1,'bar' : 2,'baz' : 3}"))
+  (is (= (js #{:foo :bar :baz}) "{'foo' : true,'bar' : true,'baz' : true}"))
+  (is (= (js [:foo :bar :baz]) "['foo','bar','baz']"))
+  (is (= (js #"^([a-z]*)([0-9]*)") "/^([a-z]*)([0-9]*)/"))
+  (is (= (js \newline) "'\n'"))
+  (is (= (js \a) "'a'")))
