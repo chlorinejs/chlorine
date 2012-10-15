628936b3caa1bef47c00bf653f11b3d72738daf4
Hoàng Minh Thắng <p@banphim.net>
2012-10-15 14:22:39 +0700

    add 'access', a naive js->clojurejs compiler

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A naive Clojure to Javascript translator"
   :url "http://github.com/myguidingstar/clojurejs"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [evaljs "0.1.2"]])
+                 [evaljs "0.1.2"]
+                 [cheshire "4.0.3"]])
