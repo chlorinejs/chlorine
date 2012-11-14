46bdda3c251846478a47a1a5f7a7d12ae09261ed
Hoàng Minh Thắng <p@banphim.net>
2012-11-14 13:58:41 +0700

    update dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/myguidingstar/chlorine"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [evaljs "0.1.2"]
-                 [cheshire "4.0.3"]])
+                 [pathetic "0.4.0"]])
