9397a7c6c2d613496adfa162c52815d3c8be734d
Hoàng Minh Thắng <p@banphim.net>
2013-03-07 01:55:16 +0700

    set version to 1.5.0

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject chlorine "1.6.0"
+(defproject chlorine "1.5.0"
   :description "A naive Clojure to Javascript translator"
-  :url "http://github.com/myguidingstar/chlorine"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [pathetic "0.4.0"]])
+  :url "http://github.com/chlorinejs/chlorine"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [pathetic "0.4.0" :exclusions [org.clojure/clojure]]
+                 [chlorine-utils "1.0.0"]
+                 [org.clojure/tools.reader "0.7.2"]])
