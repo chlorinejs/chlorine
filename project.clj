2c7b82c05250c36cc2e414535a7dfee2ba6d784c
Hoàng Minh Thắng <p@banphim.net>
2013-03-17 21:03:14 +0700

    add hiccup and clojure.walk

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [pathetic "0.4.0" :exclusions [org.clojure/clojure]]
                  [chlorine-utils "1.1.0"]
+                 [hiccup "1.0.2"]
                  [org.clojure/tools.reader "0.7.2"]])
