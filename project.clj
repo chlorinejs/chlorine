340a1d4ebd6e6ecdc9879242ac0023ee40cb8674
Hoàng Minh Thắng <p@banphim.net>
2013-07-15 02:15:33 +0700

    bump to v1.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chlorine "1.5.4"
+(defproject chlorine "1.6.0"
   :description "A naive Clojure to Javascript translator"
   :url "http://github.com/chlorinejs/chlorine"
   :dependencies [[org.clojure/clojure "1.5.1"]
