3836c1be1df4d4fc5b337d5a35acbb50c3c99010
Hoàng Minh Thắng <p@banphim.net>
2014-01-31 09:51:30 +0700

    update deps, rename to chlorine/js

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject chlorine "1.6.4-SNAPSHOT"
+(defproject chlorine/js "1.6.4-SNAPSHOT"
   :description "A naive Clojure to Javascript translator"
   :url "http://github.com/chlorinejs/chlorine"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [pathetic "0.4.0" :exclusions [org.clojure/clojure]]
                  [chlorine-utils "1.2.0"]
-                 [hiccup "1.0.3"]
+                 [hiccup "1.0.5"]
                  [slingshot "0.10.3"]
-                 [org.clojure/tools.reader "0.7.4"]])
+                 [org.clojure/tools.reader "0.8.3"]])
